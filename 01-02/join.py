32327b7a80aadc4bc12909f1cc744ce14e5cd3a6
Di Di <ddi0168@gmail.com>
2016-04-13 03:26:07 -0400

    Add join.py

---
+++ 01-02/join.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+
+def test_run():
+    # Define test range
+    start_date = '2010-01-22'
+    end_date = '2010-01-26'
+    dates = pd.date_range(start_date, end_date)
+
+    # Create an empty dataframe
+    df1 = pd.DataFrame(index=dates)
+
+    # Read SPY data into temporary dataframe
+    dfSPY = pd.read_csv("../data/SPY.csv", index_col="Date",
+                        parse_dates=True, usecols=['Date', 'Adj CLose'],
+                        na_values=['nan'])
+
+    # Join the two dataframes using DataFrame.join(), with how='inner'
+    df1 = df1.join(dfSpy, how='inner')
+
+    # Read in more stocks
+    symbols = ['AAPL']
+    for symbol in symbols:
+        df_temp = pd.read_csv("../data/[].csv".format(symbol), index_col="Date",
+                              parse_dates=True, usecols=['Date', 'Adj Close'],
+                              na_values=['nan'])
+        # rename to prevent clash
+        df_temp = df_temp.rename(columns={'Adj Close' : symbol})
+        df1=df1.join(df_temp) #use default how='left'
+
+    print df1
+
+
+if __name__ == "__main__":
+    test_run()
